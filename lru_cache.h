58d46a7502cbd1bffd3f35cbfe32768c445ae7cb
ugurgumus <ugurgumus@gmail.com>
2015-04-22 16:01:31 +0300

    comments added

--- lru_cache.h
+++ lru_cache.h
@@ -2,7 +2,7 @@
  * lru_cache.h
  *
  *  Created on: Apr 22, 2015
- *      Author: ugumus
+ *  Author: ugumus
  */
 
 #ifndef LRU_CACHE_H_
@@ -15,6 +15,9 @@
 
 namespace gms
 {
+    //
+    // exception class
+    //
     class cache_exception : public std::exception
     {
     public:
@@ -25,28 +28,45 @@ namespace gms
         std::string ex_str_;
     };
 
+    //
+    // Generic Least Recently Used(LRU) cache implementation.
+    //
     template<class key_type, class value_type>
     class lru_cache
     {
     public:
 
+        // 
+        // contruct cache with capacity
+        //
         lru_cache(unsigned capacity)
         : capacity_(capacity)
         {
         }
 
+        //
+        // destructor
+        //
         ~lru_cache(){}
 
+        //
+        // returns value of specified key, if the key exists in the cache, otherwise throws an exception
+        //
         value_type get(key_type const& key)
         {
             typename cache_type::iterator it = cache_.find(key);
+            
+            // key found
             if (it != cache_.end())
             {
+                // make first item in the list. 
                 item_list_.splice(item_list_.begin(), item_list_, it->second);
 
+                // return the value of the key
                 return it->second->second;
             }
 
+            // key does not exist. throw an exception
             throw cache_exception("key not found");
         }
 
@@ -61,12 +81,17 @@ namespace gms
                 // lru_cache reached max capacity
                 if (item_list_.size() == capacity_)
                 {
+                    // remove LRU item in the cache
                     cache_.erase(item_list_.back().first);
+
+                    // remove LRU item in the list
                     item_list_.pop_back();
                 }
 
+                // insert the new key, value pair at the begining of the list
                 item_list_.push_front(std::make_pair(key, value));
 
+                // point the new item in the cache
                 cache_[key] = item_list_.begin();
 
             }
@@ -80,11 +105,17 @@ namespace gms
             }
         }
 
+        //
+        // returns capacity of the cache
+        //
         unsigned const& get_capacity() const
         {
             return capacity_;
         }
 
+        //
+        // returns the size of the cache
+        //
         unsigned const& get_size() const
         {
             return item_list_.size();
